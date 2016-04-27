0410ef1ba596ec081f917d11e5d686870d8c7997
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-27 18:09:20 +0100

    attack feature and person unit tests GREEN

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -23,7 +23,7 @@ feature 'Attacking' do
   scenario 'reduce Maras HP by 10 points' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).not_to have_content 'Mittens: 60HP'
-    expect(page).to have_content 'Mittens: 50HP'
+    expect(page).not_to have_content 'Mara: 60HP'
+    expect(page).to have_content 'Mara: 50HP'
   end
 end
